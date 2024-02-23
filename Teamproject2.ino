42070d6113b17b77d3426caee9a6671652fa7e64
Sadayu Dachdachasunan <93990764+ppoohh25@users.noreply.github.com>
2024-02-23 21:38:24 +0700

    Add files via upload

---
+++ Teamproject2.ino
@@ -0,0 +1,104 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+char text[500];
+char n[50];
+char l[50];
+JsonDocument doc;
+// WiFi
+const char *ssid = "Suwan 19463"; // Enter your WiFi name
+const char *password = "Suda*54321";  // Enter WiFi password
+
+// MQTT Broker
+const char *mqtt_broker = "iiot.dynu.com";
+const char *topic = "65011070/test";
+const char *mqtt_username = "mosquitto";
+const char *mqtt_password = "teamproject";
+const int mqtt_port = 6688;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#include "DHT.h"
+
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+ Serial.begin(115200);
+ Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+ // connecting to a WiFi network
+ WiFi.begin(ssid, password);
+ while (WiFi.status() != WL_CONNECTED) {
+     delay(500);
+     Serial.println("Connecting to WiFi..");
+ }
+ Serial.println("Connected to the WiFi network");
+ //connecting to a mqtt broker
+ client.setServer(mqtt_broker, mqtt_port);
+ client.setCallback(callback);
+ while (!client.connected()) {
+     String client_id = "esp32-client-";
+     client_id += String(WiFi.macAddress());
+     Serial.printf("The client %s connects to the public mqtt broker\n", client_id.c_str());
+     if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+         Serial.println("Public emqx mqtt broker connected");
+     } else {
+         Serial.print("failed with state ");
+         Serial.print(client.state());
+         delay(2000);
+     }
+ }
+ // publish and subscribe
+ client.publish(topic, "Hi 65011070 Connected! :D");
+ client.subscribe(topic);
+}
+
+void callback(char *topic, byte *payload, unsigned int length) {
+ Serial.print("Message arrived in topic: ");
+ Serial.println(topic);
+ Serial.print("Message:");
+ for (int i = 0; i < length; i++) {
+     Serial.print((char) payload[i]);
+ }
+ Serial.println();
+ Serial.println("-----------------------");
+}
+
+void loop() {// Wait a few seconds between measurements.
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t)){
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  dtostrf(h,3,0,n);
+  dtostrf(t,3,0,l);
+	sprintf(text,"{\"temp\":\"%s\",\"hum\":\"%s\"}",l,n);
+  char* json = text;
+  DeserializationError error = deserializeJson(doc, json);
+  if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return;
+  }
+  client.publish(topic, text);
+  delay(1000);
+ client.loop();
+}
